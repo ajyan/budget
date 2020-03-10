023466f7260c4b3a9c1c90763fba514ad04f3c2a
Zachary Luther <Zluther89@gmail.com>
2020-03-10 15:17:37 -0400

    established db models and connection

--- db/index.js
+++ db/index.js
@@ -4,3 +4,29 @@ const Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost/budgetfuntimes", {
   useNewUrlParser: true
 });
+
+const budgetSchema = new Schema({
+  Income: Number,
+  Bills: Number,
+  Savings: Number
+});
+
+const budget = mongoose.model("budget", budgetSchema);
+
+const logSchema = new Schema({
+  Category: String,
+  Name: String,
+  Price: Number,
+  Date: Date
+});
+
+const log = mongoose.model("log", logSchema);
+
+let newLogTest = new log({
+  Category: "Fun",
+  Name: "1,000 beers",
+  Price: 5,
+  Date: Date.now()
+});
+
+newLogTest.save();
